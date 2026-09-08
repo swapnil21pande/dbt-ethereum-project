89accbc959fba06c9c279b3d896ad030a3f81d2e
swapnil pande <swapnil.pande@globalpay.com>
2026-09-08 15:51:33 +0530

    Slim CI changes

--- models/staging/analytics/stg_transaction_enriched.sql
+++ models/staging/analytics/stg_transaction_enriched.sql
@@ -23,7 +23,7 @@ t.receipt_contract_address,
 t.input,
 tt.token_transfer_count,
 1 as new_field,
-
+2 as new_field_2,
 case
     when t.receipt_contract_address != '' then 'contract_creation'
     when tt.transaction_hash is not null then 'token_transfer'
